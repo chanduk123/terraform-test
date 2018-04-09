9c5f28aa52b550b6300b93e544fbe1b264e91cf6
mavrick202 <mavrick202@gmail.com>
2018-04-09 13:40:51 +0000

    Delete lock.json
    Delete terraform-provider-aws_v1.14.0_x4
    
    Delete terraform-provider-aws_v1.13.0_x4.exe
    
    Delete lock.json
    
    Removed S3 backend
    
    Added Data Source for AMI
    
    Added packer.json
    
    Update packer.json
    
    Update packer.json
    
    Update main.tf
    
    Update outputs.tf
    
    Resolved Tag Error with "="
    
    Resolved Tag Error with "="
    
    Update packer.json
    
    Update main.tf
    
    Update test.tfvars
    
    Rename outputs.tf to outputs.tfx
    
    Create jenkibuildscript
    
    Create docker.service
    
    Update jenkibuildscript
    
    Update main.tf
    
    Update main.tf
    
    Update docker.service
    
    Update packer.json
    
    Update main.tf
    
    Update packer.json
    
    Update main.tf
    
    Update main.tf
    
    Rename test.tfvars to terraform.tfvars
    
    Update main.tf
    
    Update main.tf
    
    Update main.tf
    
    Update main.tf
    
    Update main.tf
    
    Update main.tf
    
    Update main.tf
    
    Update packer.json
    
    Update packer.json
    
    Update main.tf
    
    Update main.tf
    
    Update main.tf
    
    Update main.tf
    
    Update main.tf
    
    Update main.tf
    
    Update main.tf
    
    Update main.tf
    
    Update main.tf
    
    Update main.tf
    
    Update main.tf
    
    Update packer.json
    
    Update packer.json
    
    Update packer.json
    
    Update packer.json
    
    Update packer.json
    
    Update packer.json
    
    Update main.tf
    
    Update variables.tf
    
    Update variables.tf
    
    Update variables.tf
    
    Update main.tf
    
    Update main.tf
    
    Update main.tf
    
    Create ami.txt
    
    Rename ami.txt to ami.txtx
    
    Update packer.json
    
    Create packer-vars.json
    
    Update main.tf
    
    Update main.tf
    
    Update main.tf
    
    Update variables.tf
    
    Update main.tf
    
    Update packer-vars.json
    
    Update main.tf
    
    Update main.tf
    
    Update packer.json
    
    Update packer-vars.json
    
    Update main.tf
    
    deleting not req files for test
    
    deleting not required files
    
    Delete terraform.exe
    
     downladed terraform
    
    updating repo after class1
    
     final commit after class 1
    
    last final commit after class1
    
    deleted tf.exe large file
    
    new final comit
    
    final fix to push to repo
    
    base commit

---
+++ vpc.tf
@@ -0,0 +1,19 @@
+
+resource "aws_vpc" "default" {
+    cidr_block = "${var.vpc_cidr}"
+    enable_dns_hostnames = true
+    tags = {
+        Name = "${var.vpc_name}"
+	Owner = "Sreeharsha Veerapalli"
+	environment = "${var.environment}"
+    moviesname = var.moviesname
+    leadactor = var.leadactor
+    }
+}
+
+resource "aws_internet_gateway" "default" {
+    vpc_id = "${aws_vpc.default.id}"
+	tags = {
+        Name = "${var.IGW_name}"
+    }
+}
